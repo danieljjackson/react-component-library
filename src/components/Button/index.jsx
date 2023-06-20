fbc1d0751b52f7f483c7eb14872b4d85ca6da22c
justtoconfirm <office@justtoconfirm.co.uk>
2023-06-20 17:46:19 +0100

    feat: initial update to the Button component to render as a Link if to prop is used

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,18 @@
 import React from "react"
 
-const Button = () => {
-    return (
+import Link from "./../Link"
+
+const Button = ({ to }) => {
+    const isLink = to && (to.includes(`http`) || to.startsWith(`#`) || to.startsWith(`mailto`) || to.startsWith(`/`))
+
+    const renderAsLink = () =>
+        <Link to={to}>Link</Link>
+
+    const renderAsButton = () =>
         <button>Button</button>
+
+    return (
+        isLink ? renderAsLink() : renderAsButton()
     )
 }
 
