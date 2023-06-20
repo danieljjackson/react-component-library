884eaaf9dd4c6799c204615c9ccd19f95d0bb040
justtoconfirm <office@justtoconfirm.co.uk>
2023-06-20 17:56:06 +0100

    feat: default role prop value of button applied

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 import Link from "./../Link/index.jsx"
 
-const Button = ({ to, type = "button", role, onClick, children, ...others }) => {
+const Button = ({ to, type = "button", role = "button", onClick, children, ...others }) => {
     const isLink = to && (to.includes(`http`) || to.startsWith(`#`) || to.startsWith(`mailto`) || to.startsWith(`/`))
 
     const renderAsLink = () =>
