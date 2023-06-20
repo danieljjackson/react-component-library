fd1845d1cd11e4f136a447cee91a8c047d5deb51
justtoconfirm <office@justtoconfirm.co.uk>
2023-06-20 17:55:08 +0100

    feat: add the role prop to the Button and Link components

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,11 +2,11 @@ import React from "react"
 
 import Link from "./../Link/index.jsx"
 
-const Button = ({ to, type = "button", onClick, children, ...others }) => {
+const Button = ({ to, type = "button", role, onClick, children, ...others }) => {
     const isLink = to && (to.includes(`http`) || to.startsWith(`#`) || to.startsWith(`mailto`) || to.startsWith(`/`))
 
     const renderAsLink = () =>
-        <Link to={to}>Link</Link>
+        <Link {...{ to, role }}>Link</Link>
 
     const renderAsButton = () =>
         <button {...{ type, onClick }} {...others}>Button</button>
