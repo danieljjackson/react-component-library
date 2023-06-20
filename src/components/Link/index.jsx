5876baa7bc4863ca061ca182ca88d0bf8d3a76eb
danieljjackson <128699318+danieljjackson@users.noreply.github.com>
2023-06-20 17:14:29 +0100

    feat: link component added (#4)
    * feat: add whitespace within the Heading component
    
    * feat: initial Link component added
    
    * feat: initial props added to the Link component
    
    * feat: set the rel prop if the link is external
    
    * feat: update the rel prop to use a spread operator
    
    ---------
    
    Co-authored-by: justtoconfirm <office@justtoconfirm.co.uk>

---
+++ src/components/Link/index.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Link = ({ to, openAsTab = false, children, ...others }) => {
+    let rel = ""
+
+    // Check to see if the link is external
+    if ((to && to.startsWith(`http`)) || to.startsWith(`mailto`)) {
+        rel = "noopener noreferrer"
+    }
+
+    return (
+        <a href={to} target={openAsTab ? "blank" : undefined} {...{ rel }} {...others}>{children}</a>
+    )
+}
+
+export default Link
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as Button } from './components/Button/index.jsx'
 export { default as Heading } from './components/Heading/index.jsx'
+export { default as Link } from './components/Link/index.jsx'
