61b8030bb64d03256584f7cb5b329e857a2cd78f
justtoconfirm <office@justtoconfirm.co.uk>
2023-06-20 17:58:48 +0100

    feat: add the openAsTab prop to the Button component for when it renders as a link

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,11 +2,11 @@ import React from "react"
 
 import Link from "./../Link/index.jsx"
 
-const Button = ({ to, type = "button", role = "button", onClick, children, ...others }) => {
+const Button = ({ to, type = "button", role = "button", openAsTab = false, onClick, children, ...others }) => {
     const isLink = to && (to.includes(`http`) || to.startsWith(`#`) || to.startsWith(`mailto`) || to.startsWith(`/`))
 
     const renderAsLink = () =>
-        <Link {...{ to, role }}>Link</Link>
+        <Link {...{ to, role, openAsTab }}>Link</Link>
 
     const renderAsButton = () =>
         <button {...{ type, onClick }} {...others}>Button</button>
