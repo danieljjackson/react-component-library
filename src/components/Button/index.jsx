15edacbee9a3fbdc30be6b50780671109865c719
danieljjackson <128699318+danieljjackson@users.noreply.github.com>
2023-06-20 17:59:41 +0100

    feat: button component updated and renders as a link when a URL is passed (#5)
    * feat: initial update to the Button component to render as a Link if to prop is used
    
    * feat: fix applied for the import path to the Link component inside of the Button component
    
    * feat: extend the props to the Button component
    
    * feat: add the role prop to the Button and Link components
    
    * feat: default role prop value of button applied
    
    * feat: add the openAsTab prop to the Button component for when it renders as a link
    
    ---------
    
    Co-authored-by: justtoconfirm <office@justtoconfirm.co.uk>

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,18 @@
 import React from "react"
 
-const Button = () => {
+import Link from "./../Link/index.jsx"
+
+const Button = ({ to, type = "button", role = "button", openAsTab = false, onClick, children, ...others }) => {
+    const isLink = to && (to.includes(`http`) || to.startsWith(`#`) || to.startsWith(`mailto`) || to.startsWith(`/`))
+
+    const renderAsLink = () =>
+        <Link {...{ to, role, openAsTab }}>Link</Link>
+
+    const renderAsButton = () =>
+        <button {...{ type, onClick }} {...others}>Button</button>
+
     return (
-        <button>Button</button>
+        isLink ? renderAsLink() : renderAsButton()
     )
 }
 
