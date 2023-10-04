4201844fb94b87430b2f6ccd54a705ed11f2809d
justtoconfirm <office@justtoconfirm.co.uk>
2023-10-04 15:10:25 +0100

    feat: conditionally render classes within the Button, Link and Heading components

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,14 +2,14 @@ import React from "react"
 
 import Link from "./../Link/index.jsx"
 
-const Button = ({ to, type = "button", role = "button", openAsTab = false, onClick, children, ...others }) => {
+const Button = ({ to, className, type = "button", role = "button", openAsTab = false, onClick, children, ...others }) => {
     const isLink = to && (to.includes(`http`) || to.startsWith(`#`) || to.startsWith(`mailto`) || to.startsWith(`/`))
 
     const renderAsLink = () =>
-        <Link {...{ to, role, openAsTab }} {...others}>{children}</Link>
+        <Link {...{ to, role, className, openAsTab }} {...others}>{children}</Link>
 
     const renderAsButton = () =>
-        <button {...{ type, onClick }} {...others}>{children}</button>
+        <button {...{ type, onClick }} className={`${className} || ""}`} {...others}>{children}</button>
 
     return (
         isLink ? renderAsLink() : renderAsButton()
