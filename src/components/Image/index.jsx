ff4ebf84b5c3b3ae53c6120f7a44da9df7209ddc
justtoconfirm <office@justtoconfirm.co.uk>
2023-11-10 16:37:06 +0000

    feat: add the others prop to the image component

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 import Link from "../Link/"
 
-const Image = ({ src, srcset, alt, className, id, style, height, width, to, openAsTab = false, isLazyLoading = false }) => {
+const Image = ({ src, srcset, alt, className, id, style, height, width, to, openAsTab = false, isLazyLoading = false, ...others }) => {
     return to ? (
         <Link {...{ to, openAsTab }}>
-            <img {...{ src, srcset, alt, className, id, style, height, width }} loading={isLazyLoading ? "lazy" : undefined} />
+            <img {...{ src, srcset, alt, className, id, style, height, width }} loading={isLazyLoading ? "lazy" : undefined} {...others} />
         </Link>
     ) : (
-        <img {...{ src, srcset, alt, className, id, style, height, width }} loading={isLazyLoading ? "lazy" : undefined} />
+        <img {...{ src, srcset, alt, className, id, style, height, width }} loading={isLazyLoading ? "lazy" : undefined} {...others} />
     )
 }
 
