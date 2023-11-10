b966f6175ebeda7a42ded184ed2a2a624b28b674
justtoconfirm <office@justtoconfirm.co.uk>
2023-11-10 16:32:13 +0000

    feat: ternary operator used to render the image inside a link if the to prop is used and a value is passed

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 
-const Image = ({ src, srcset, alt, className, id, style, height, width, isLazyLoading = false }) => {
-    return (
+const Image = ({ src, srcset, alt, className, id, style, height, width, to, isLazyLoading = false }) => {
+    return to ? (
+        <a href={to}>
+            <img {...{ src, srcset, alt, className, id, style, height, width }} loading={isLazyLoading ? "lazy" : undefined} />
+        </a>
+    ) : (
         <img {...{ src, srcset, alt, className, id, style, height, width }} loading={isLazyLoading ? "lazy" : undefined} />
     )
 }
