138d6495bb07dc9b7488a4a98f65df5264b07ba2
justtoconfirm <office@justtoconfirm.co.uk>
2023-11-10 16:39:29 +0000

    feat: create the imageProps object to avoid duplication of the props used in the image component

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -2,12 +2,25 @@ import React from "react"
 import Link from "../Link/"
 
 const Image = ({ src, srcset, alt, className, id, style, height, width, to, openAsTab = false, isLazyLoading = false, ...others }) => {
+    const imageProps = {
+        src,
+        srcset,
+        alt,
+        className,
+        id,
+        style,
+        height,
+        width,
+        loading: isLazyLoading ? "lazy" : undefined,
+        ...others,
+    }
+
     return to ? (
         <Link {...{ to, openAsTab }}>
-            <img {...{ src, srcset, alt, className, id, style, height, width }} loading={isLazyLoading ? "lazy" : undefined} {...others} />
+            <img {...imageProps} />
         </Link>
     ) : (
-        <img {...{ src, srcset, alt, className, id, style, height, width }} loading={isLazyLoading ? "lazy" : undefined} {...others} />
+        <img {...imageProps} />
     )
 }
 
